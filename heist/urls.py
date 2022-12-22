425fb7d1e71e12bb824fa5dc1bc82f652878d0e8
Ayush <ayush.krsh9@gmail.com>
2022-12-22 20:30:53 +0530

    last commit2

--- heist/urls.py
+++ heist/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views as teleport
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',teleport.home),
     path('search',teleport.search),
     path('contact', teleport.contact),
-]
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
